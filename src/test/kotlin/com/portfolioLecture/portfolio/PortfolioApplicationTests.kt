e02d6c0b9eca46741da8483b371255967bc837eb
cpu624049 <kjs940426@naver.com>
2024-10-04 15:06:00 +0900

    [미션2] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/test/kotlin/com/portfolioLecture/portfolio/PortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.portfolioLecture.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
